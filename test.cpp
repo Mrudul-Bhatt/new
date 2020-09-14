055fbd825044390b3326f577419adb67949e8599
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2020-09-14 17:56:00 +0530

    second commit

---
+++ test.cpp
@@ -0,0 +1,19 @@
+/*
+pwd
+ls
+cd
+mkdir
+touch
+echo
+git init
+git status
+git add .
+git commit -m "first commit"
+git push 
+git pull
+git branch
+git branch feature 
+git checkout feature
+git branch -D feature
+*/
+//changes
